2c9e83843f098b75c2d04d0d26c7a2e08df406ff
Carlos F. Ferry <carlos.ferry@gmail.com>
2017-10-09 13:42:30 -0300

    Update thread.cpp

--- src/thread.cpp
+++ src/thread.cpp
@@ -1,10 +1,35 @@
 #include "uthreads.h"
 #include "thread.h"
+#include "arch.h"
 
 Thread::Thread() 
 {
-      sigsetjmp(env, 1);
-      runtime = 1;
+        sigsetjmp(env, 1);
+        runtime = 1;
+}
+
+Thread::Thread(void (*args)())
+{
+        /* Initialize runtime */
+        
+        runtime = 0;
+
+        /* Initialize stack and sp */
+        
+        stack = new char[STACKSIZE];
+
+        /* Initializes env buffer **/
+        
+        sigsetjmp(env, 1);
+
+        (env->__jmpbuf)[JB_SP] = translate_address((address_t)stack + STACKSIZE-sizeof(address_t));
+        (env->__jmpbuf)[JB_PC] = translate_address((address_t)args);
+}
+
+
+void Thread::IncrementRun()
+{
+      this->runtime++;
 }
 
 Thread::~Thread()
@@ -12,12 +37,42 @@ Thread::~Thread()
       delete stack;
       stack = NULL;
 }
-void Thread::IncrementRun()
-{
-      this->runtime++;
-}
 
-Thread::operator GetBuffer&()
+
+Thread::operator sigjmp_buf&()
 {
         return this->env;
+}
+
+void Thread::SetSleepCounter(int sleeptime)
+{
+        
+        /* Sleep counter must be a positive int */
+        
+        if (sleeptime < 0)
+        {
+                return;
+        }
+
+        Thread::SleepCounter = sleeptime;
+}
+
+int Thread::GetSleepCounter() const
+{
+        return SleepCounter;
+}
+
+
+void Thread::DecrementSleepCounter()
+{
+        if (this->SleepCounter <= 0)
+        {
+                this->SleepCounter = 0;
+                return;
+        }
+
+        this->SleepCounter--;
+        
+        Instance->Logs->Log("Sleep counter set to: " + this->SleepCounter);
+        
 }
