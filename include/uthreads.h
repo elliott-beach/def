a7076e9d32e6d385f45f2e60d94533a891dfae57
Carlos F. Ferry <carlos.ferry@gmail.com>
2017-10-09 13:41:41 -0300

    Update uthreads.h

--- include/uthreads.h
+++ include/uthreads.h
@@ -13,11 +13,8 @@
 
 class Uthreads;
 class Thread;
-class Scheduler;
 
-extern Uthreads* Instance;
-
-/* Stack size to be used by each thread (size is in bytes) */
+/* Stack size to be used by each thread. Size is in bytes */
 
 #define STACKSIZE 4096 
 
@@ -25,13 +22,72 @@ extern Uthreads* Instance;
 
 #define MAXTHREADS 100
 
-#include "arch.h"
+#include "logger.h"
 #include "scheduler.h"
 #include "thread.h"
+
+extern Uthreads* Instance;
 
 class Uthreads
 {
    private:
            
           std::map<int, Thread*> ThreadMap;
+
+          /* timer struct val */
+           
+          struct itimerval time_struct;
+          
+          int SigCounter;
+          
+          /* Tests final flag */
+          
+          bool FinalFlag;
+          
+          /* Lock flag. Set to true when a test is taking place. */
+          
+          bool Locked;
+
+   public:
+   
+          Uthreads();
+          
+          ~Uthreads();
+          
+          Logger* Logs = NULL;
+          
+          void RunTest();
+          
+          void Initialize();
+
+          void SpawnScheduler();
+
+          void SignalHandler(int Signal);
+
+          void SchedulerConfigure();
+          
+          void SchedulerSwitch();
+          
+          std::map<int, Thread*> GetThreads()
+          {
+                return this->ThreadMap;
+          }
+      
+          bool GetFlag()
+          {
+              return this->Locked;
+          }
+          
+          int uthread_get_tid();
+          
+          /* 
+           * Returns number of quantums with provided id
+           * will be running. "0" will be returned if no thread is found.
+           * Otherwise, this function will return number of quantums until
+           * it wakes up
+           */
+ 
+           int CalculaeUntilQuantum(int TID);
+
+
 };
