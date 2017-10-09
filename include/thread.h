6df25eaa7a37b613d05b5d36b8fa213d00ee4ff6
Carlos F. Ferry <carlos.ferry@gmail.com>
2017-10-09 13:41:17 -0300

    Update thread.h

--- include/thread.h
+++ include/thread.h
@@ -1,45 +1,49 @@
 #pragma once
 
-/* Stack size to be used by each thread. Size is in bytes */
-
-#define STACKSIZE 4096 
-
 class Thread
 {
     private:
     
         /* Stack's storage */
-
+        
         char *stack;
-
+        
         int SleepCounter;
-
+        
         unsigned int runtime;
 
         /* Thread's environment buffer */
         
         sigjmp_buf env;
-    
+        
     public:
-
+    
         Thread();
-    
+        
+        /* Default destructor */
+        
         ~Thread();
-
+        
         Thread(void (*args)());
-    
+       
         /* How many times has this thread ran? */
         
-        unsigned int GetRuntimes() const
+        unsigned int GetRuntime() const
         {
               return this->runtime;   
         }
-    
+
         void IncrementRun();
 
+        void SetSleepCounter(int sleeptime);
+
+        int GetSleepCounter() const;
+        
+        void DecrementSleepCounter();
+
         /* Returns a reference to the thread's sigjmp_buf buffer */
-
-        operator GetBuffer&();
-
-
+        
+        operator sigjmp_buf&();
+        
+        
 };
