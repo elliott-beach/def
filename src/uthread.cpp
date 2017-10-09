0707888683b91310cec027f9a30cc6595be9e057
Carlos F. Ferry <carlos.ferry@gmail.com>
2017-10-09 13:42:41 -0300

    Delete uthread.cpp

--- src/uthread.cpp
+++
@@ -1,6 +0,0 @@
-#include <cstddef>
-size_t async_read(int filedes, void *buf, size_t nbytes){
-    // Start polling in scheduler.
-    // Call thread_yield()
-    // Reschedule when the read is complete.
-}
